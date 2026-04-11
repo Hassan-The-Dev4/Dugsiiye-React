633add37f0583c154f3ed8777afadb81854db939
hassangnt18@gmail.com <hassangnt18@gmail.com>
2026-04-11 19:15:48 +0300

    submitted exercise 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import UserList from "./UserList";
 import { Counterdown } from "./Counterdown";
 import { Greeting } from "./Greeting";
 import { MouseTracker } from "./MouseTracker";
 import { UserFetching } from "./UserFetching";
 
 function App() {
-
+  
+    const users = [
+      { id: 1, name: 'Abdirahman', email: 'abdirahman@gmail.com.com' },
+      { id: 2, name: 'Hassan', email: 'Hassan12@gmail.com' },
+    ];
 
   return (
 
@@ -13,7 +17,12 @@ function App() {
       // <EventResize />
       // <MouseTracker />
       //  <Counterdown />
-      <UserFetching />
+      // <UserFetching />
+
+      <div>
+        <UserList users={users} />
+      </div>
+
   )
 
 
